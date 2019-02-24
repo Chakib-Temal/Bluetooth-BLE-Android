21afa9ec24ceac9a14f55e7d45bbbd218ea1a47b
Chakib-Temal <chakibtemal@gmail.com>
2019-02-24 20:37:14 +0100

    firstClean

---
+++ app/src/main/java/com/example/bluetoothtest/Models/permissions/MarshlowPermissions.java
@@ -0,0 +1,80 @@
+package com.example.bluetoothtest.Models;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarshlowPermissions {
+
+    Activity activity;
+    public final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 50;
+
+    public static MarshlowPermissions getInstance(Activity activity){
+        return new MarshlowPermissions(activity);
+    }
+
+    public MarshlowPermissions(Activity activity){
+        this.activity = activity;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void fuckMarshMallow() {
+        List<String> permissionsNeeded = new ArrayList<String>();
+
+        final List<String> permissionsList = new ArrayList<String>();
+        if (!addPermission(permissionsList, Manifest.permission.ACCESS_FINE_LOCATION))
+            permissionsNeeded.add("Show Location");
+
+        if (permissionsList.size() > 0) {
+            if (permissionsNeeded.size() > 0) {
+                // Need Rationale
+                String message = "App need access to " + permissionsNeeded.get(0);
+
+                for (int i = 1; i < permissionsNeeded.size(); i++)
+                    message = message + ", " + permissionsNeeded.get(i);
+                showMessageOKCancel(message,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                activity.requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+                            }
+                        });
+                return;
+            }
+            activity.requestPermissions(permissionsList.toArray(new String[permissionsList.size()]), REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+            return;
+        }
+        Toast.makeText(activity, "No new Permission Required- Launching App .You are Awesome!!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(activity)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private boolean addPermission(List<String> permissionsList, String permission) {
+
+        if (activity.checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+            // Check for Rationale Option
+            if (!activity.shouldShowRequestPermissionRationale(permission))
+                return false;
+        }
+        return true;
+    }
+
+}
